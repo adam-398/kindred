78a6d0ea1d8f50c3379d243b74114f2215bdd7d3
adam <afhodges@pm.me>
2026-04-22 18:20:00 +0200

    changed Gemini model to gemini-3.1-flash-lite-preview, recomendations now working

--- app/src/main/java/com/example/kindred/GeminiAPI/GeminiService.kt
+++ app/src/main/java/com/example/kindred/GeminiAPI/GeminiService.kt
@@ -46,7 +46,7 @@ suspend fun getGeminiSuggestions(
 
     Log.d("GeminiTest", "API Key: ${BuildConfig.GEMINI_API_KEY}")
 
-    val url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${BuildConfig.GEMINI_API_KEY}"
+    val url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key=${BuildConfig.GEMINI_API_KEY}"
     Log.d("GeminiTest", "URL: $url")
 
     val client = OkHttpClient.Builder()
